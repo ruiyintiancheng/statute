df587bf9b97264f9fd316cee74c2a63a602aca2b
beigeyishui <1961232369@qq.com>
2020-01-17 09:11:23 +0800

    保存图片

--- src/views/relationChart/triangleChart/components/chart/util.js
+++ src/views/relationChart/triangleChart/components/chart/util.js
@@ -1,3 +1,5 @@
+import saveSvg from 'save-svg-as-png'
+
 class Util {
   static deepMix(defultObj, newObj) {
     for (const i in newObj) {
@@ -42,33 +44,48 @@ class Util {
 
     var svg = cfg.svg
     const bg = cfg.g.node().getBBox()
+    const width = bg.width + 2 * padding
+    const height = bg.height + 2 * padding
+
     const csvg = svg.clone(true)
     csvg.select('g.group').attr('transform', `translate(${padding - bg.x}, ${padding - bg.y})scale(1)`)
 
-    var serializer = new XMLSerializer()
-    var source = '<?xml version="1.0" standalone="no"?>\r\n' + serializer.serializeToString(csvg.node())
-
-    csvg.remove()
-
-    var image = new Image()
-    image.src = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(source)
-
-    var canvas = document.createElement('canvas')
-    canvas.width = bg.width + 2 * padding
-    canvas.height = bg.height + 2 * padding
-
-    var context = canvas.getContext('2d')
-    context.fillStyle = '#fff'// #fff设置保存后的PNG 是白色的
-    context.fillRect(0, 0, 10000, 10000)
-
-    image.onload = function() {
-      context.drawImage(image, 0, 0)
-      var a = document.createElement('a')
-      a.download = `${title}.png`
-      a.href = canvas.toDataURL('image/png')
-      a.click()
-    }
+    saveSvg.saveSvgAsPng(csvg.node(), `${title}.png`, { height: height, width: width })
   }
+
+  // static savePng(cfg, title) {
+  //   title = title || '图谱'
+  //   const padding = 50
+
+  //   var svg = cfg.svg
+  //   const bg = cfg.g.node().getBBox()
+  //   const csvg = svg.clone(true)
+  //   csvg.select('g.group').attr('transform', `translate(${padding - bg.x}, ${padding - bg.y})scale(1)`)
+
+  //   var serializer = new XMLSerializer()
+  //   var source = '<?xml version="1.0" standalone="no"?>\r\n' + serializer.serializeToString(csvg.node())
+
+  //   csvg.remove()
+
+  //   var image = new Image()
+  //   image.src = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(source)
+
+  //   var canvas = document.createElement('canvas')
+  //   canvas.width = bg.width + 2 * padding
+  //   canvas.height = bg.height + 2 * padding
+
+  //   var context = canvas.getContext('2d')
+  //   context.fillStyle = '#fff'// #fff设置保存后的PNG 是白色的
+  //   context.fillRect(0, 0, 10000, 10000)
+
+  //   image.onload = function() {
+  //     context.drawImage(image, 0, 0)
+  //     var a = document.createElement('a')
+  //     a.download = `${title}.png`
+  //     a.href = canvas.toDataURL('image/png')
+  //     a.click()
+  //   }
+  // }
 }
 
 export default Util
