a3fd1c78e35945e3c1ddbc9d538f6a3799d3fcc7
beigeyishui <1961232369@qq.com>
2020-03-04 16:46:53 +0800

    bug修改

--- src/router/index.js
+++ src/router/index.js
@@ -61,7 +61,8 @@ export const constantRouterMap = [
       {
         name: 'analysis',
         path: '/analysis',
-        component: _import('analysis/serach')
+        component: _import('searchResult/index')
+        // component: _import('analysis/index')
       }
     ]
   },
