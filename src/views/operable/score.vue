04045b86a02f9c9fa84de4fe709c7db164d53894
李昆 <likun@fascte.com>
2020-04-28 18:23:10 +0800

    '代码提交'

--- src/views/operable/score.vue
+++ src/views/operable/score.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="score clearfix">
-        <h2 class="score-title">
-            分值: 
-            <span class="score-value">{{fraction}}</span>
-            <a class="score-back" @click="$router.go(-1)">返回</a>
-        </h2>
+        <!-- <el-scrollbar style="height:100%;" > -->
+        <div class="score-header">
+          <h2 class="score-title">
+              分值: 
+              <span class="score-value">{{fraction}}</span>
+              <a class="score-back" @click="$router.go(-1)">返回</a>
+          </h2>
+        </div>
         <div class="score-container first">
           <div class="score-name">{{$route.query.sourceFileName}}</div>
           <div class="score-text" v-html="sourceContent"></div>
         </div>
-        <div class="score-container last" >
-          <div class="score-name">{{$route.query.sourceFileName}}</div>
-          <div class="score-text" v-html="targetContent"></div>
-        </div>
+        <!-- <div class="score-container last" > -->
+          <!-- <div class="score-name">分值</div> -->
+          <!-- <div class="score-text" v-html="targetContent"></div> -->
+        <!-- </div> -->
+        <!-- </el-scrollbar> -->
     </div>
 </template>
 <script>
@@ -29,6 +33,9 @@ export default {
     this.$nextTick(_ => {
       this.getData()
     })
+    window.addEventListener('resize', function() {
+      this.getData()
+    }.bind(this), false)
   },
   methods: {
     getData() {
@@ -72,75 +79,90 @@ export default {
       })
       // 生成公文
       let sourceContent = ''
-      let targetContent = ''
+      // let targetContent = ''
       let sourceIndex = 0
-      const mainWidth = document.querySelector('.score').offsetWidth
+      // const mainWidth = document.querySelector('.score').offsetWidth
       const containerWidth = document.querySelector('.score-container').offsetWidth
-      const jj = mainWidth * 0.04 - 10
+      // const jj = mainWidth * 0.04 - 10
       sourceArray.forEach(d => {
         // const sourceId = d.sourceId
         const itemScore = (d.SCORE * 100).toFixed(2)
         sourceContent += sourceFile.substring(sourceIndex, d.OUT_SOURCE_START)
-        targetContent += `<span style="opacity:0;">${sourceFile.substring(sourceIndex, d.OUT_SOURCE_START)}</span>`
+        // targetContent += `<span style="opacity:0;">${sourceFile.substring(sourceIndex, d.OUT_SOURCE_START)}</span>`
         const offsetLeft = 10
         // if (/^\s/.test(d.OUT_SOURCE_CONTENT)) {
         //   offsetLeft = 40
         // }
-        sourceContent += `<span style="color: rgb(3, 126, 251);position:relative;">${sourceFile.substring(d.OUT_SOURCE_START, d.OUT_SOURCE_END)}<span style="position: absolute;top: -4px;left: ${offsetLeft}px;width: ${containerWidth + jj}px;height: 6px;border: 1px solid rgb(49, 100, 183);border-bottom: none;"><i style="font-size: 12px;top: -10px;position: absolute;right: 3px;color: red;">${itemScore}</i></span></span>`
-        targetContent += `<span style="color: rgb(3, 126, 251);position:relative;">${sourceFile.substring(d.OUT_SOURCE_START, d.OUT_SOURCE_END)}</span>`
+        sourceContent += `<span style="color: rgb(3, 126, 251);position:relative;">${sourceFile.substring(d.OUT_SOURCE_START, d.OUT_SOURCE_END)}<span style="position: absolute;top: -4px;left: ${offsetLeft}px;width: ${containerWidth}px;height: 6px;border: 1px solid rgb(49, 100, 183);border-bottom: none;"><i style="font-size: 12px;position: absolute;right: -16px;color: #F56C6C;font-style:normal;"> ${itemScore}</i></span></span>`
+        // targetContent += `<span style="color: rgb(3, 126, 251);position:relative;">${sourceFile.substring(d.OUT_SOURCE_START, d.OUT_SOURCE_END)}</span>`
         sourceIndex = d.OUT_SOURCE_END
       })
       sourceContent += sourceFile.slice(sourceIndex)
-      targetContent += `<span style="opacity:0;">${sourceFile.slice(sourceIndex)}</span>`
+      // targetContent += `<span style="opacity:0;">${sourceFile.slice(sourceIndex)}</span>`
       this.sourceContent = sourceContent
-      this.targetContent = targetContent
+      // this.targetContent = targetContent
     }
   }
 }
 </script>
 <style lang="scss" scoped>
     .score {
-        padding:30px;
-        // height:100%;
+        // padding-bottom:30px;
+        height:100%;
+        // overflow-x: hidden;
+        .score-header{
+          height: 60px;
+          width:100%;
+          line-height: 60px;
+          // background-color: #DCDFE6;
+          // position:absolute;
+          // top:60px;
+        }
         .score-title{
           // position: relative;
+          // margin-left:15%;
+          width: 50%;
+          margin:0px auto;
+          color:#606266;
           .score-value{
-            color:red;
+            color:#F56C6C;
           }
           .score-back{
-            position: absolute;
+            // position: absolute;
             color:rgb(3, 126, 251);
             font-size: 14px;
-            right: 100px;
-            top:80px;
+            // right: 100px;
+            // top:60px;
+            float: right;
           }
         }
         .score-container{
-            margin-top:10px;
-            margin-bottom:30px;
-            width: 45%;
-            float:left;
-            height: calc(100% - 50px);
+          // overflow-x: hidden;
+            // margin-top:10px;
+            // margin-bottom:30px;
+            width: 50%;
+            margin:0px auto 30px;
+            // float:left;
+            min-height: calc(100% - 90px);
             white-space: pre-line; 
             font-size: 14px; 
-            line-height: 150%;
+            // line-height: 150%;
+            line-height: 28px;
             padding: 10px 30px 20px;
             // color: gray;
             // overflow: auto;
             color:gray;
             &.first{
+              //  margin-left:15%;
                background-color: #fff;
                box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
                .score-name{
                 border-bottom:1px solid #DCDFE6;
                }
             }
-            &.last{
-                margin-left:4%;
-                .score-name{
-                  opacity:0;
-                }
-            }
+            // &.last{
+            //     // margin-left:4%;
+            // }
             .score-name{
               padding: 5px;
               color:#666;
@@ -154,4 +176,14 @@ export default {
             }
         }
     }
+</style>
+<style lang="scss">
+// .el-scrollbar .el-scrollbar__wrap .el-scrollbar__view{
+//    white-space: nowrap;
+// }
+.score{
+  .el-scrollbar__wrap {
+  overflow-x: hidden;
+  }
+}
 </style>
