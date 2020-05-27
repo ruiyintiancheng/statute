f8327762dcc321fe551bd6bb555530477449f055
Wang Kuo <bigk22668@163.com>
2020-05-27 18:08:31 +0800

    模型管理

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,8 +3,8 @@ module.exports = {
   ENV_CONFIG: '"dev"',
   CHART_API: '"http://123.56.175.133:8080/modelRunningServer"',
   // BASE_API: '"https://api-dev"'
-  // BASE_API: '"http://47.93.121.177:8080/app/mock/36//"'
+  BASE_API: '"http://39.96.94.120:38080/app/mock/36//"'
   // BASE_API: '"http://39.106.123.32:8080/cmprsNewBack"'
-  BASE_API: '"http://123.56.175.133:8080/cmprs"'
+  // BASE_API: '"http://123.56.175.133:8080/cmprs"'
   // BASE_API: '"http://192.168.3.159:8082/"'
 }
