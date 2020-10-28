baa31bd5e140fcb8c321e97d2e7ad35fb899305a
Vincy <448468237@qq.com>
2020-10-28 16:18:43 +0800

    dddddd

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,9 +1,9 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  CHART_API: '"http://123.56.175.133:8080/modelRunningServer"',
+  CHART_API: '"http://123.57.226.204:8080/modelRunningServer"',
   // BASE_API: '"https://api-dev"'
   // BASE_API: '"http://39.96.94.120:38080/app/mock/36//"'
-  BASE_API: '"http://123.56.175.133:8080/cmprs"'
+  BASE_API: '"http://123.57.226.204:8080/cmprs"'
   // BASE_API: '"http://192.168.3.159:8082/"'
 }
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,10 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  CHART_API: '"http://123.56.175.133:8080/modelRunningServer"',
+  CHART_API: '"http://123.57.226.204:8080/modelRunningServer"',
   // BASE_API: '"https://api-prod"'
   // BASE_API: '"http://47.93.121.177:8080/app/mock/17/"'
   // BASE_API: '"http://39.106.123.32:8080/cmprsNewBack"'
-    BASE_API: '"http://123.56.175.133:8080/cmprs"'
+    BASE_API: '"http://123.57.226.204:8080/cmprs"'
   // BASE_API: '"http://localhost:8082/"'
 }
